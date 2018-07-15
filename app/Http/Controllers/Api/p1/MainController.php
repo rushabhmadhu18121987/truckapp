f026acc13bbd731fc4814e0f1d409759945dc208
Rushabh Madhu <rushabh.madhu@innvonix.com>
2018-07-15 12:14:42 +0530

    Logout and Make API working and create folder structure for api

---
+++ app/Http/Controllers/Api/p1/MainController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Api\p1;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    //
+}
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,16 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login()
+    {   $data = array('test'=>"Welcome");
+        return response()->json($data);
+    }
+
+    public function logout(Request $request) {
+        $this->guard()->logout();
+        $request->session()->invalidate();
+        return redirect('/');       
     }
 }
