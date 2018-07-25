6906764f04b1324d8423712e2c00b6588de2c174
Hitesh Makodiya <hitesh.makodiya55@gmail.com>
2018-07-26 00:28:15 +0530

    changes

--- resources/views/layouts/includes/menu.blade.php
+++ resources/views/layouts/includes/menu.blade.php
@@ -25,17 +25,17 @@
 
             <!-- Main -->
             <li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
-            <li class="active"><a href="{{url('home')}}"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
-            <li>
+            <li <?php echo (Request::segment(1) == 'home') ? 'class="active"' : ""; ?>><a href="{{url('home')}}"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
+            <li <?php echo (Request::segment(1) == 'users') ? 'class="active"' : ""; ?>>
                 <a href="#"><i class="icon-copy"></i> <span>Users</span></a>
                 <ul>
-                  <li><a href="{{ url('users') }}" id="layout1">All Users</a></li>
+                  <li <?php echo (Request::segment(1) == 'users') ? 'class="active"' : ""; ?>><a href="{{ url('users') }}" id="layout1">All Users</a></li>
                     <!-- <li><a href="{{ url('lenders') }}" id="layout1">Lenders</a></li>
                     <li><a href="{{ url('customers') }}">Customers </a></li> -->
                 </ul>
             </li>
-            <li><a href="{{ url('subscribers') }}"><i class="icon-droplet2"></i> <span>Subscribers</span></a></li>
-            <li><a href="{{ url('vehicleCat') }}"><i class="icon-droplet2"></i> <span>Vehicle Category</span></a></li>
+            <li <?php echo (Request::segment(1) == 'subscribers') ? 'class="active"' : ""; ?>><a href="{{ url('subscribers') }}"><i class="icon-droplet2"></i> <span>Subscribers</span></a></li>
+            <li <?php echo (Request::segment(1) == 'vehicleCat') ? 'class="active"' : ""; ?>><a href="{{ url('vehicleCat') }}"><i class="icon-droplet2"></i> <span>Vehicle Category</span></a></li>
             <li><a href="#"><i class="icon-droplet2"></i> <span>Plans</span></a></li>
             <li><a href="#"><i class="icon-stack"></i> <span>Orders</span></a></li>
             <li><a href="#"><i class="icon-pencil3"></i> <span>Account Settings</span></a></li>
