f46b3ad3f109bcf4137a3dcf800d49c0c2f72737
Hitesh Makodiya <hitesh.makodiya55@gmail.com>
2018-07-28 13:19:41 +0530

    upload menu change

--- resources/views/layouts/includes/menu.blade.php
+++ resources/views/layouts/includes/menu.blade.php
@@ -27,12 +27,8 @@
             <li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
             <li <?php echo (Request::segment(1) == 'home') ? 'class="active"' : ""; ?>><a href="{{url('home')}}"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
             <li <?php echo (Request::segment(1) == 'users') ? 'class="active"' : ""; ?>>
-                <a href="#"><i class="icon-copy"></i> <span>Users</span></a>
-                <ul>
-                  <li <?php echo (Request::segment(1) == 'users') ? 'class="active"' : ""; ?>><a href="{{ url('users') }}" id="layout1">All Users</a></li>
-                    <!-- <li><a href="{{ url('lenders') }}" id="layout1">Lenders</a></li>
-                    <li><a href="{{ url('customers') }}">Customers </a></li> -->
-                </ul>
+                <a href="{{ url('users') }}"><i class="icon-copy"></i> <span>Users</span></a>
+               
             </li>
             <li <?php echo (Request::segment(1) == 'subscribers') ? 'class="active"' : ""; ?>><a href="{{ url('subscribers') }}"><i class="icon-droplet2"></i> <span>Subscribers</span></a></li>
             <li <?php echo (Request::segment(1) == 'vehicleCat') ? 'class="active"' : ""; ?>><a href="{{ url('vehicleCat') }}"><i class="icon-droplet2"></i> <span>Vehicle Category</span></a></li>
