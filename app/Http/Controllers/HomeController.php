57bc0cc6dc37c47825c9bab8bdc45d56ed6d0646
Rushabh Madhu <rushabhmadhu@gmail.com>
2018-07-31 09:10:20 +0530

    changes in dashboad counts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,13 +25,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $users = User::count();
+	$users = User::count();
         $subscribers = User::where('is_subscribers','1')->count();
         $vehicles = DB::table('vehicles')->count();
         $category = DB::table('category')->count();
-        $orders = 0;
-        $completed_order = 0;
-        $canceled_order = 0;
-        return view('dashboard',compact('users','subscribers','vehicles','category','orders','completed_order','canceled_order'));
+        $orders = DB::table('orders')->count();
+        $completed_order = DB::table('orders')->count();
+        $canceled_order = DB::table('orders')->where('status','2')->count();
+	    $promocodes = DB::table('promocodes')->where('status','1')->count();
+        return view('dashboard',compact('users','subscribers','vehicles','promocodes','category','orders','completed_order','canceled_order'));
     }
 }
